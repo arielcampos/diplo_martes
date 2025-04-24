3de3fc0bce7f34446467388f9260d527047dfdf3
Ari Campos <33043437+arielcampos@users.noreply.github.com>
2025-04-24 16:14:17 -0300

    agregando cloudinary

---
+++ proyecto_utn/back/models/novedadesModel.js
@@ -0,0 +1,52 @@
+var pool = require('./bd');
+var md5 = require('md5');
+
+async function getNovedades() {
+    try {
+        var query = 'select * from novedades';
+        var rows = await pool.query(query);
+        return rows;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function insertNovedad(obj) {
+    try {
+        var query = "insert into novedades set ?";
+        var rows = await pool.query(query, [obj])
+        return rows;
+
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+async function deleteNovedadesById(id) {
+    var query = "delete from novedades where id = ?";
+    var rows = await pool.query(query, [id]);
+    return rows;
+}
+
+
+async function getNovedadesById(id) {
+    var query = "select * from novedades where id = ? ";
+    var rows = await pool.query(query, [id]);
+    return rows[0];
+}
+
+async function modificarNovedadById(obj, id) {
+    try {
+        var query = "update novedades set ? where id=?";
+        var rows = await pool.query(query, [obj, id]);
+        return rows;
+    } catch (error) {
+        throw error;
+    }
+}
+
+
+
+module.exports = { getNovedades, insertNovedad, deleteNovedadesById, getNovedadesById,modificarNovedadById }
+
--- proyecto_utn/back/routes/admin/novedades.js
+++ proyecto_utn/back/routes/admin/novedades.js
@@ -1,13 +1,124 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require("../../models/novedadesModel");
+var util = require('util');
+var cloudinary = require('cloudinary').v2;
+const uploader = util.promisify(cloudinary.uploader.upload);
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+
+router.get('/', async function(req, res, next) {
+  
+  var novedades = await novedadesModel.getNovedades();
+
+novedades = novedades.map(novedad => {
+  if (novedad.img_id) {
+    const imagen = cloudinary.image(novedad.img_id, {
+      width: 100,
+      height: 100,
+      crop: 'fill'
+    });
+    return {
+      ...novedad,
+      imagen
+    }
+}
+    else {
+      return {
+        ...novedad,
+        imagen: ''
+      }
+    } 
+});
+
   res.render('admin/novedades',{
     layout:'admin/layout',
-    persona : req.session.nombre
+    persona : req.session.nombre,
+    novedades
   });
   
 });
+/*para  mostrar formulario de agregar*/
+
+ router.get('/agregar',(req,res,next)=> {
+  res.render('admin/agregar', { 
+    layout: 'admin/layout'
+
+  })
+ });
+
+router.post('/agregar', async (req,res,next) => {
+  try {
+      var img_id = '';
+      
+      if (req.files && Object.keys(req.files).lenght>0)
+      {
+        imagen = req.files.imagen;
+        img_id = (await uploader(imagen.tempFilePath)).public_id;
+      }
+
+    if (req.body.titulo != "" && req.body.subtitulo != "") {
+      await novedadesModel.insertNovedad({
+        ...req.body,
+        img_id
+      });
+
+      res.redirect('/admin/novedades')
+    } else {
+      res.render('admin/agregar',{
+        layout: 'admin/layout',
+      error: true,
+      message: 'todos los campos son requeridos'
+    })
+    }
+    } catch (error) {
+      console.log(error)
+      res.render('admin/agregar',{
+        layout: 'admin/layout',
+        error: true,
+        message : 'No se cargo la novedad'
+      })
+    }
+ }
+)
+
+router.get('/eliminar/:id', async (req, res, next)=> {
+  var id = req.params.id;
+  await novedadesModel.deleteNovedadesById(id);
+  res.redirect('/admin/novedades');
+});
+
+
+router.get('/modificar/:id', async (req,res,next) => {
+  var id = req.params.id;
+  var novedad = await novedadesModel.getNovedadesById(id);
+
+  res.render('admin/modificar', {
+    layout: 'admin/layout',
+    novedad
+  });
+});
+
+router.post ('/modificar', async (req,res,next) => {
+  try {
+    var obj = {
+      titulo: req.body.titulo,
+      subtitulo: req.body.subtitulo
+    }
+    console.log(obj);
+    await novedadesModel.modificarNovedadById(obj,req.body.id);
+    res.redirect('/admin/novedades');
+  } catch (error) {
+    console.log(error);
+    res.render('admin/modificar' , {
+      layout: 'admin/layout',
+      error: true,
+      message: ' No se modifico la entrada al portfolio'
+    })
+  }
+  })
+
+
+
+
 
 module.exports = router;
