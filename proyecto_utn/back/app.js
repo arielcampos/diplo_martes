c5656a832be7b6728d8f3355be1555bea12f15b2
Ari Campos <33043437+arielcampos@users.noreply.github.com>
2025-04-27 22:06:03 -0300

    agregue la conexion con react

--- proyecto_utn/back/app.js
+++ proyecto_utn/back/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 require('dotenv').config();
 var session = require('express-session');
@@ -13,7 +14,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/admin/login');
 var adminRouter = require('./routes/admin/novedades');
-
+var apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -58,6 +59,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin/login',loginRouter);
 app.use('/admin/novedades', secured, adminRouter);
+app.use('/api', cors(), apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -74,5 +76,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
--- proyecto_utn/front/src/pages/Testimonios.js
+++ proyecto_utn/front/src/pages/Testimonios.js
@@ -1,46 +1,36 @@
 import '../styles/components/pages/Testimonios.css'
 
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import NovedadItem from '../components/layout/novedades/NovedadItem';
+
 const Testimonios = (props) => {
+
+    const [loading, setLoading] = useState(false);
+    const [novedades, setNovedades] = useState([]);
+
+    useEffect(() => {
+        const cargarNovedades = async () => {
+            setLoading(true);
+            const response = await axios.get('http://localhost:3000/api/novedades');
+            setNovedades(response.data);
+            setLoading(false);
+        };
+
+        cargarNovedades();
+    }, []);
+
     return (
-        <>
-            <div className="testimonios">
-                <h2>Testimionios</h2>
-                <div className="novedades">
-                    <h3>Cliente</h3>
-                    <h4>Subtitulo</h4>
-                    <p> Comentario - Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quas eos perspiciatis
-                        velit cum cupiditate tempore assumenda soluta ut doloribus molestias magnam expedita, quidem quam
-                        maiores a dignissimos! Tenetur, facilis?</p>
-
-
-                </div><div className="novedades">
-                    <h3>Cliente</h3>
-                    <h4>Subtitulo</h4>
-                    <p> Comentario - Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quas eos perspiciatis
-                        velit cum cupiditate tempore assumenda soluta ut doloribus molestias magnam expedita, quidem quam
-                        maiores a dignissimos! Tenetur, facilis?</p>
-
-
-                </div><div className="novedades">
-                    <h3>Cliente</h3>
-                    <h4>Subtitulo</h4>
-                    <p> Comentario - Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quas eos perspiciatis
-                        velit cum cupiditate tempore assumenda soluta ut doloribus molestias magnam expedita, quidem quam
-                        maiores a dignissimos! Tenetur, facilis?</p>
-
-
-                </div><div className="novedades">
-                    <h3>Cliente</h3>
-                    <h4>Subtitulo</h4>
-                    <p> Comentario - Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quas eos perspiciatis
-                        velit cum cupiditate tempore assumenda soluta ut doloribus molestias magnam expedita, quidem quam
-                        maiores a dignissimos! Tenetur, facilis?</p>
-
-
-                </div>
-            </div>
-
-        </>
+        <section className="holder">
+            <h2>Testimonios</h2>
+            {loading ? (
+                <p>Cargando...</p>
+            ) : (
+                novedades.map(item => <NovedadItem key={item.id}
+                    title={item.titulo} subtitle={item.subtitulo}
+                    imagen={item.imagen} body={item.cuerpo} />)
+            )}
+        </section>
     );
 }
 
